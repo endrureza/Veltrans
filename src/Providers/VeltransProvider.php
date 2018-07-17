21de94646f0146648877f360e002a5d93104dc8f
Endru Reza <lotusb13@gmail.com>
2018-07-17 17:57:03 +0700

    Init composer.json

---
+++ src/Providers/VeltransProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the endru/velitrans package. 
+ * 
+ * (c) Endru Reza <lotusb13@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ * 
+ * @see https://github.com/endrureza/veltrans
+ */
+
+namespace Endru\Veltrans\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class VeltransProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot() 
+    {
+
+    }
+}
